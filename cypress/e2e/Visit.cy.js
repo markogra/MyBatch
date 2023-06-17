c11e019141948cf8e1177c4078a8f60161b33542
Caian Keyes <caiankeyes@gmail.com>
2023-06-17 12:58:58 +0100

    feat: cypress testing for inventory

--- cypress/e2e/Visit.cy.js
+++ cypress/e2e/Visit.cy.js
@@ -1,5 +1,25 @@
 describe('My First Test', () => {
   it('Visits MyBatch', () => {
     cy.visit('http://localhost:3000/');
+
+    cy.contains('Welcome to MyBatch Home Page').should('be.visible');
+
+    cy.contains('Inventory').click();
+
+    cy.get('.hops-dd').select('Fuggle');
+
+    cy.get('input').first().type('3');
+
+    cy.contains('Add').click();
+
+    cy.reload();
+
+    cy.contains('Fuggle 3').should('be.visible');
+
+    cy.get('.deleteButton').first().click();
+
+    cy.contains('Fuggle 3').should('not.exist');
+
+    cy.contains('Our recipes').click();
   });
 });
