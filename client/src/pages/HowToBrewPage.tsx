898f9ab84e43790e2902774061b285634898142e
Caian Keyes <caiankeyes@gmail.com>
2023-06-19 14:07:35 +0100

    feat: how to brew page added

--- client/src/pages/HowToBrewPage.tsx
+++ client/src/pages/HowToBrewPage.tsx
@@ -3,8 +3,58 @@ import React from "react";
 function HowToBrew() {
   return (
     <div>
-      <h1>This is how to brew page where you can read on how to brew</h1>
+      <h1>How to get Started Brewing your own Beer</h1>
+      
+      <div className="box">
+        <h2 className="title">Understanding the Basics</h2>
+        <p className="text">Before diving into brewing, it's important to grasp the fundamental concepts. Beer is made from four primary ingredients: malted grains(Malts), hops, yeast, and water. Malts provide fermentable sugars, hops contribute bitterness and aroma, yeast converts sugars to alcohol, and water forms the base of the beer.</p>
+      </div>
+
+      <div className="box">
+        <h2 className="title">Gathering Equipment</h2>
+        <p className="text">To begin brewing, you'll need some basic equipment. This includes a fermenter, brew kettle, airlock, hydrometer, thermometer, and sanitizing solution. These tools are essential for a successful brewing experience and can be found at local brewing supply stores or online.</p>
+      </div>
+
+      <div className="box">
+        <h2 className="title">Recipe Selection</h2>
+        <p className="text">Selecting a recipe is an exciting part of the brewing process. Consider starting with a beer style that suits your taste preferences. Research different recipes and experiment with various malt profiles, hop combinations, and yeast strains. We provide a list of great and begginer friendly recipes in the ourRecipes section.</p>
+      </div>
+
+      <div className="box">
+        <h2 className="title">Sanitization</h2>
+        <p className="text">Sanitization is crucial to prevent contamination during the brewing process. Clean and sanitize all your brewing equipment thoroughly before use. Any residual bacteria or unwanted microorganisms can negatively impact the flavor and quality of your beer.</p>
+      </div>
+
+      <div className="box">
+        <h2 className="title">Mashing and Boiling</h2>
+        <p className="text">Mashing is the process of mixing malts with hot water to extract fermentable sugars. Heat the water to the appropriate temperature, add the grains, and maintain the temperature within a specified range. This allows enzymes in the grains to convert starches into sugars. Once the mashing is complete, proceed to the boiling phase.</p>
+      </div>
+
+      <div className="box">
+        <h2 className="title">Hops and Boil Additions</h2>
+        <p className="text">During the boiling phase, hops are added to the wort (the liquid extracted from the grains). Hops provide bitterness, flavor, and aroma to the beer. Different hop additions at various times during the boil can create a range of flavors and aromas. Follow the recipe instructions for the precise timing and quantity of hop additions.</p>
+      </div>
+
+      <div className="box">
+        <h2 className="title">Fermentation</h2>
+        <p className="text">After boiling, cool the wort rapidly to the desired temperature and transfer it to a fermentation vessel. Add yeast to the cooled wort, which will consume the sugars and produce alcohol and carbon dioxide. Place an airlock on the fermentation vessel to allow the carbon dioxide to escape while preventing external contaminants from entering.</p>
+      </div>
+
+      <div className="box">
+        <h2 className="title">Conditioning and Carbonation</h2>
+        <p className="text">Once primary fermentation is complete, the beer enters the conditioning phase. During this time, flavors mellow, and the beer undergoes further maturation. After conditioning, it's time to carbonate the beer. Carbonation can be achieved through bottle conditioning or force carbonation using kegs.</p>
+      </div>
+
+      <div className="box">
+        <h2 className="title">Patience and Enjoyment</h2>
+        <p className="text">Brewing beer is a labor of love that requires patience. Allow sufficient time for conditioning and carbonation before sampling your homemade brew. Each batch may improve with time, so resist the temptation to rush the process. Gather friends and fellow beer enthusiasts to share and enjoy the fruits of your labor.</p>
+      </div>
+
+      <div className="box">
+        <p className="text">Embarking on the journey of brewing your own beer opens up a world of creativity, flavors, and community. With the right equipment, basic knowledge, and a sense of adventure, you can craft unique beers tailored to your taste. Remember to start with simple recipes and gradually explore more complex techniques. Cheers to the joy of brewing your own beer!</p>
+      </div>
     </div>
+    
   );
 }
 
