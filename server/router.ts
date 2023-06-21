4b6ba055236ece453216f28d18d673118086a3db
Caian Keyes <caiankeyes@gmail.com>
2023-06-21 10:08:55 +0100

    feat: added express validation to my recipe

--- server/router.ts
+++ server/router.ts
@@ -7,6 +7,7 @@ import { postMyRecipe } from './controllers/postMyRecipe';
 import { postOurRecipe } from './controllers/postOurRecipe';
 import express from 'express';
 import { ingredientValidator } from './validators/ingredientValidator';
+import { myRecipeValidator } from './validators/myRecipeValidator';
 
 const router = express.Router();
 
@@ -15,7 +16,7 @@ router.post("/inventory", ingredientValidator, createIngredients);
 router.delete("/inventory/:id", deleteIngredient);
 router.get("/our-recipes", getOurRecipes);
 router.get("/my-recipes", getMyRecipes);
-router.post("/my-recipes", postMyRecipe);
+router.post("/my-recipes", myRecipeValidator, postMyRecipe);
 router.post('/our-recipes', postOurRecipe);
 
 export { router };
