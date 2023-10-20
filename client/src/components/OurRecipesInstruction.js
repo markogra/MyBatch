3fb6e4bf0bc24053dedc83c7d729d745a48fa256
Marko Grahovac <m_grahovac@yahoo.com>
2023-10-20 12:58:08 +0200

    our recipe page refactored

---
+++ client/src/components/OurRecipesInstruction.js
@@ -0,0 +1,37 @@
+export default function OurRecipesInstruction({ selectedRecipe }) {
+  return (
+    <div className="recipe-instruction containers">
+      <h2>Details</h2>
+      {selectedRecipe && (
+        <div className="ing-details">
+          <h3>Hops</h3>
+          <ul className="ing-ul">
+            {selectedRecipe.ingredients.hops.map((hop) => (
+              <li key={hop._id}>
+                {hop.name} {hop.amount} Adding time: {hop.time}
+              </li>
+            ))}
+          </ul>
+          <h3>Yeast</h3>
+          <ul className="ing-ul">
+            <li>{selectedRecipe.ingredients.yeast}</li>
+          </ul>
+          <h3>Malts</h3>
+          <ul className="ing-ul">
+            {selectedRecipe.ingredients.malts.map((malt) => (
+              <li key={malt._id}>
+                {malt.name} {malt.amount}
+              </li>
+            ))}
+          </ul>
+          <h2>Recipe instructions</h2>
+          <ul>
+            {selectedRecipe.instructions.map((instruction) => (
+              <li key={instruction}>{instruction}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ client/src/components/OurRecipesList.js
@@ -0,0 +1,27 @@
+export default function OurRecipesList({
+  allRecipes,
+  handleRecipeClick,
+  selectedRecipe,
+}) {
+  return (
+    <div className="recipe-list containers">
+      <h2>Here are some one our most popular recipes</h2>
+      <div className="scrollable-list">
+        {allRecipes && (
+          <ul className="ourRecipes">
+            {allRecipes.map((recipe) => (
+              <li
+                key={recipe._id}
+                onClick={() => handleRecipeClick(recipe)}
+                className={selectedRecipe === recipe ? "active-recipe" : ""}
+              >
+                <h3>{recipe.name}</h3>
+                <p>{recipe.description}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+}
