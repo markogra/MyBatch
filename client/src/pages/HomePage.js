eb53b8dde67ad41f603759d417c7700feb463b9c
Marko Grahovac <m_grahovac@yahoo.com>
2023-10-31 16:28:57 +0100

    homepage design

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -10,22 +10,25 @@ function Homepage() {
         homebrewers with crafting their perfect brews and managing their
         ingredient stock.
       </h2>
-      <p>
-        Explore a vast collection of recipes sourced from experienced
-        homebrewers worldwide or create your own unique recipes from scratch.
-        With MyBatch, the recipe creation process is simplified and streamlined.
-      </p>
-      <p className="second">
-        Seamlessly browse through a diverse range of hops, malts, yeasts, and
-        more, and effortlessly add them to your inventory for easy tracking.
-        MyBatch ensures you never run out of essential ingredients.
-      </p>
-      <p>
-        Unlock your creativity with MyBatch's intuitive interface, where you can
-        experiment with different beer styles, customize instructions, and
-        fine-tune ingredient quantities to achieve the desired flavors and
-        aromas.
-      </p>
+      <div className="p-container">
+        <p>
+          Explore a vast collection of recipes sourced from experienced
+          homebrewers worldwide or create your own unique recipes from scratch.
+          With MyBatch, the recipe creation process is simplified and
+          streamlined.
+        </p>
+        <p>
+          Seamlessly browse through a diverse range of hops, malts, yeasts, and
+          more, and effortlessly add them to your inventory for easy tracking.
+          MyBatch ensures you never run out of essential ingredients.
+        </p>
+        <p>
+          Unlock your creativity with MyBatch's intuitive interface, where you
+          can experiment with different beer styles, customize instructions, and
+          fine-tune ingredient quantities to achieve the desired flavors and
+          aromas.
+        </p>
+      </div>
     </div>
   );
 }
