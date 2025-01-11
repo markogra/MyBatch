407bf479cde822f2f0a92e40bd117fb152a1666c
markogra <m_grahovac@yahoo.com>
2025-01-11 18:18:51 +0100

    db conn fix 2

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
+require('dotenv').config()
+const {letsConnect} = require('./models/db')
 
-const PORT = 3500;
 const router = require("./router");
 const cors = require("cors");
 
@@ -9,6 +10,9 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(PORT, function () {
-  console.log("MyBatch server listening on port " + PORT);
+// Connecting to DB
+letsConnect()
+
+app.listen( process.env.PORT, function () {
+  console.log("MyBatch server listening on port " + process.env.PORT);
 });
--- server/models/db.js
+++ server/models/db.js
@@ -8,6 +8,4 @@ function letsConnect() {
   );
 }
 
-letsConnect();
-
-module.exports = mongoose;
+module.exports = {letsConnect};
